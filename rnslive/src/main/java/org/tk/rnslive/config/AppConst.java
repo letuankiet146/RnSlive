06ee3bff4e492ca3461a2f726b8aba4c7b0ee6a9
kiet.le <letuankiet146@gmail.com>
2026-03-12 20:05:22 +0700

    remove constant for hardcode exchange

--- rnslive/src/main/java/org/tk/rnslive/config/AppConst.java
+++
@@ -1,7 +0,0 @@
-package org.tk.rnslive.config;
-
-import org.ltk.connector.client.ExchangeName;
-
-public class AppConst {
-    public static final ExchangeName EXCHANGE_NAME = ExchangeName.BINANCE;
-}
